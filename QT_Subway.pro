5800afffef9e9bb24c38bfc17bb727c5fbd566c3
Shen <310632220@qq.com>
2019-09-22 20:12:43 +0800

    创建项目

---
+++ QT_Subway.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2019-09-20T21:43:01
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = QT_Subway
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    subwaygraph.cpp
+
+HEADERS  += mainwindow.h \
+    subwaygraph.h
+
+FORMS    += mainwindow.ui
---
+++ subwaygraph.h
@@ -0,0 +1,19 @@
+﻿#ifndef SUBWAYGRAPH_H
+#define SUBWAYGRAPH_H
+#include<QFile>
+#include<QString>
+class SubwayGraph
+{
+public:
+    SubwayGraph();
+    ~SubwayGraph();
+protected:
+    QVector<Station> stations;          //存储所有站点
+    QVector<Line> lines;                //存储所有线路
+    QHash<QString, int> stationsHash;   //站点名到存储位置的 hash
+    QHash<QString, int> linesHash;      //线路名到存储位置的 hash
+    QSet<Edge> edges;                   //所有边的集合
+    QVector<QVector<Node>> graph;       //地铁线路网络图
+};
+
+#endif // SUBWAYGRAPH_H
